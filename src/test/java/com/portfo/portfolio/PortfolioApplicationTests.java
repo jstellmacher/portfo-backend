799496a936b88db1459e07a469c98314be2d47cd
jai <jaijai.stell@gmail.com>
2024-06-10 15:08:40 -0700

    Initial commit

---
+++ src/test/java/com/portfo/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfo.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
